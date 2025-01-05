54701b96343035c203ff83ae6259b61aec0fcb25
Aidan <aidan@p0ntus.com>
2025-01-05 16:49:15 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import '@fontsource-variable/manrope';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
